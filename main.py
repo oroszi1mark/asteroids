da9c990a7522e818b0ec03d6176f15f38bb11062
Mark Oroszi <8012978+oroszi1mark@users.noreply.github.com>
2025-09-08 22:03:16 +0200

    add delta time

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 
 
