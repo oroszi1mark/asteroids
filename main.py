82ff77f40dc891f39d1ede7906a719afe90821b5
Mark Oroszi <8012978+oroszi1mark@users.noreply.github.com>
2025-09-08 22:34:38 +0200

    create sprite groups

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     clock = pygame.time.Clock()
     delta_time = 0
+
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(delta_time)
-        player.draw(screen)
+        updatable.update(delta_time)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         delta_time = clock.tick(60) / 1000
 
